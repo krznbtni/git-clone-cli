2154c0a138bdab016c354bcfd3e4db214d1ee786
Karzan Botani <karzan@botani.nu>
2023-04-10 18:14:03 +0200

    feat: initial complete program

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,131 @@
-fn main() {
-    println!("Hello, world!");
+use std::collections::HashMap;
+use std::error::Error;
+use std::path::PathBuf;
+use std::process::Command;
+
+use console::Style;
+use dialoguer::{theme::ColorfulTheme, Input, MultiSelect};
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug)]
+#[allow(dead_code)]
+struct Cli {
+    username: String,
+    // repos: Vec<usize>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[allow(dead_code)]
+struct GhRepoRes {
+    name: String,
+    clone_url: String,
+}
+
+async fn fetch_gh_repos(username: &str) -> Result<Vec<GhRepoRes>, reqwest::Error> {
+    let url = format!("https://api.github.com/users/{}/repos", username);
+
+    let client = reqwest::Client::new();
+    let res = client
+        .get(url)
+        .header("ACCEPT", "application/vnd.github+json")
+        .header("User-Agent", username)
+        .send()
+        .await?;
+
+    match res.status() {
+        reqwest::StatusCode::OK => Ok(res.json::<Vec<GhRepoRes>>().await?),
+        reqwest::StatusCode::BAD_REQUEST => {
+            panic!("Bad request")
+        }
+        reqwest::StatusCode::NOT_FOUND => {
+            panic!("GitHub user not found")
+        }
+        rest => panic!("GitHub response: {}", rest),
+    }
+}
+
+fn remove_whitespace(s: &str) -> String {
+    s.split_whitespace().collect()
+}
+
+async fn init() -> Result<Option<Cli>, Box<dyn Error>> {
+    let theme = ColorfulTheme {
+        values_style: Style::new().yellow().dim(),
+        ..ColorfulTheme::default()
+    };
+
+    let username = Input::with_theme(&theme)
+        .with_prompt("GitHub username")
+        .validate_with({
+            move |input: &String| -> Result<(), &str> {
+                let trimmed = remove_whitespace(input);
+
+                if trimmed.is_empty() {
+                    Err("Invalid username.")
+                } else {
+                    Ok(())
+                }
+            }
+        })
+        .interact_text()?;
+
+    let mut repo_name_clone_url_map: HashMap<String, String> = HashMap::new();
+
+    let repos = fetch_gh_repos(&username).await;
+    match repos {
+        Err(..) => panic!("dsads"),
+        Ok(x) => {
+            repo_name_clone_url_map = x
+                .iter()
+                .map(|ss| (ss.name.clone(), ss.clone_url.clone()))
+                .collect();
+        }
+    }
+
+    let repo_names = repo_name_clone_url_map
+        .keys()
+        .cloned()
+        .collect::<Vec<String>>();
+
+    let repos_to_clone = MultiSelect::with_theme(&theme)
+        .with_prompt("Pick repos to clone")
+        .items(&repo_names[..])
+        .interact()
+        .unwrap();
+
+    if repos_to_clone.is_empty() {
+        panic!("No repos selected. Exiting.")
+    }
+
+    // TODO 5: give input promp - enter directory to clone repos in
+    let clone_to_dir: String = Input::with_theme(&theme)
+        .with_prompt("Directory to clone to")
+        .default(".".to_string())
+        .interact_text()?;
+
+    let proper_clone_to_dir = PathBuf::from(&clone_to_dir);
+
+    for repo in repos_to_clone {
+        let repo_name = &repo_names[repo];
+
+        match repo_name_clone_url_map.get(repo_name) {
+            Some(clone_url) => {
+                Command::new("git")
+                    .args(["clone", clone_url])
+                    .current_dir(&proper_clone_to_dir)
+                    .spawn()
+                    .expect("Failed to clone");
+            }
+            None => println!("Filler"),
+        }
+    }
+
+    Ok(Some(Cli {
+        username: username.clone(),
+    }))
+}
+
+#[tokio::main]
+async fn main() {
+    init().await;
 }
